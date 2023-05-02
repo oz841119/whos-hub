5e94ad0079ebbff16c14432b0cdc37568de204c5
oz841119 <oz841119@gmail.com>
2023-05-02 11:28:29 +0800

    none

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,20 @@
 import {Main, NextScript , Html, Head} from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
+    console.log(process.env.NODE_ENV === 'production');
     return (
       <Html>
         <Head>
+          <Script src="https://www.googletagmanager.com/gtag/js?id=G-XWFCGB25NN" strategy="afterInteractive"></Script>
+          <Script id="gtag">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag() {dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-XWFCGB25NN');
+            `}
+          </Script>
         </Head>
         <body>
           <Main/>
